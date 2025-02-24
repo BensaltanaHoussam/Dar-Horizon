7ddfed63264502a46d05368ad52feb651b8413e9
BensaltanaHoussam <bensaltna07@gmail.com>
2025-02-24 16:57:26 +0000

    breeze installations

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel</title>
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div class="flex justify-center items-center h-screen">
+        <h1 class="text-4xl font-bold">Welcome to Laravel!</h1>
+    </div>
+</body>
+</html>
